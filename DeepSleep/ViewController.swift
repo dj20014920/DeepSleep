a392d5fbe0eddc134b04febe05bee63fd214f6a2
dj20014920 <vinny4920@gmail.com>
2025-04-15 17:52:37 +0900

    Initial Commit

---
+++ DeepSleep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeepSleep
+//
+//  Created by 추동준 on 4/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DeepSleepTests/DeepSleepTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeepSleepTests.swift
+//  DeepSleepTests
+//
+//  Created by 추동준 on 4/15/25.
+//
+
+import Testing
+@testable import DeepSleep
+
+struct DeepSleepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
